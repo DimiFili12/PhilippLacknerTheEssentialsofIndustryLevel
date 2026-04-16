abbf8e4aa77829a9e181e7b73da8d99cfbb49bf8
DimiFili12 <dimifili1@gmail.com>
2026-04-16 14:57:46 +0300

    Liskov-Substitution Principle

--- app/src/main/java/com/example/philipplacknertheessentialsofindustrylevel/PaymentService.kt
+++ app/src/main/java/com/example/philipplacknertheessentialsofindustrylevel/PaymentService.kt
@@ -4,18 +4,20 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.WebSocketListener
 
-abstract class WebClient {
-    abstract fun connectToSocket(url: String)
+abstract class WebClient
+
+abstract class HttpWebClient: WebClient() {
     abstract fun get(url: String)
 }
 
-class HttpWebClient(
-    private val okHttpClient: OkHttpClient
-): WebClient() {
-    override fun connectToSocket(url: String) {
-        TODO("Not yet implemented")
-    }
+abstract class WebSocketClient: WebClient() {
+    abstract fun connectToSocket(url: String)
+}
 
+
+class HttpWebClientImpl(
+    private val okHttpClient: OkHttpClient
+): HttpWebClient() {
     override fun get(url: String) {
         val request = Request.Builder()
             .method("GET", null)
@@ -25,18 +27,13 @@ class HttpWebClient(
     }
 }
 
-class WebSocketClient(
+class WebSocketClientImpl(
     private val okHttpClient: OkHttpClient
-): WebClient() {
+): WebSocketClient() {
     override fun connectToSocket(url: String) {
         val request = Request.Builder()
             .url(url)
             .build()
         okHttpClient.newWebSocket(request, object : WebSocketListener() {})
     }
-
-    override fun get(url: String) {
-        TODO("Not yet implemented")
-    }
-
 }
