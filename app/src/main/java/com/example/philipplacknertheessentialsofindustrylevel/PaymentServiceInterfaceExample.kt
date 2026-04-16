337ff5081f610b10974c4beb897e95619ccc245b
DimiFili12 <dimifili1@gmail.com>
2026-04-16 15:03:03 +0300

    Violates Interface-Segregation Principle

---
+++ app/src/main/java/com/example/philipplacknertheessentialsofindustrylevel/PaymentServiceInterfaceExample.kt
@@ -0,0 +1,9 @@
+package com.example.philipplacknertheessentialsofindustrylevel
+
+// if you have OnClickListener you typically only care about the onClick function
+// so an interface like this violates Interface-Segregation Principle
+interface onClickListener {
+    fun onClick()
+    fun onFingerUp()
+}
+
