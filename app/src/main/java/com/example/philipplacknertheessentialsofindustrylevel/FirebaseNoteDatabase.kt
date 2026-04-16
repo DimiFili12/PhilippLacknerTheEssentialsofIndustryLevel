713785240244d62cf93683e6ff6d01ad8d50fc3f
DimiFili12 <dimifili1@gmail.com>
2026-04-16 15:27:13 +0300

    Dependency-Inversion Principle, examples

--- app/src/main/java/com/example/philipplacknertheessentialsofindustrylevel/FirebaseNoteDatabase.kt
+++ app/src/main/java/com/example/philipplacknertheessentialsofindustrylevel/FirebaseNoteDatabase.kt
@@ -9,6 +9,28 @@ interface NoteDatabase {
     suspend fun getNote(title: String): Note?
 }
 
+class NoteDatabaseFake: NoteDatabase {
+    override suspend fun saveNote(note: Note) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getNote(title: String): Note? {
+        TODO("Not yet implemented")
+    }
+}
+
+class YourCustomNoteDatabase(
+    private val api: JustAnExampleApi
+): NoteDatabase {
+    override suspend fun saveNote(note: Note) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getNote(title: String): Note? {
+        TODO("Not yet implemented")
+    }
+}
+
 class FirebaseNoteDatabase: NoteDatabase {
     val db = FirebaseFirestore.getInstance()
 
